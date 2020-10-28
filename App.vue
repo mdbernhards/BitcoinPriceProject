c2762d4ac3cf0dc437935af1a9b7b889b389a95b
Martins Davis Bernhards <49512684+mdbernhards@users.noreply.github.com>
2020-10-28 17:39:08 +0200

    added files

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img  alt="Bitcoin logo" src="./assets/BTCLogo.svg" style="height: 200px; width: 200px;">
+    <Bitcoin/>
+    <BitcoinHistory/>
+  </div>
+</template>
+
+<script>
+  import Bitcoin from './components/Bitcoin'
+  import BitcoinHistory from './components/BitcoinHistory'
+
+  export default {
+    name: 'App',
+    components: {
+      Bitcoin,
+      BitcoinHistory
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+</style>
