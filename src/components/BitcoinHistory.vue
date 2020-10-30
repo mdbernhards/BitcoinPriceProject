dde524eb445514119905dfe4ee4ee248ccd9c0fb
Martins Davis Bernhards <49512684+mdbernhards@users.noreply.github.com>
2020-10-30 14:12:13 +0200

    Graph refresh every time a button is pressed
    Graph refresh every time a button is pressed.

--- src/components/BitcoinHistory.vue
+++ src/components/BitcoinHistory.vue
@@ -3,15 +3,13 @@
         <trend-chart  v-if="weekDataset.length && isShown == 1" :datasets="[{data: weekDataset, fill: true, className: 'curve-btc'}]" :labels="weekLabels" :min="11000.00" :grid="grid" :interactive="true"/>
         <trend-chart  v-if="monthDataset.length && isShown == 2" :datasets="[{data: monthDataset, fill: true, className: 'curve-btc'}]" :labels="monthLabels" :min="7000.00" :grid="grid" :interactive="true"/>
         <trend-chart  v-if="yearDataset.length && isShown == 3" :datasets="[{data: yearDataset, fill: true, className: 'curve-btc'}]" :labels="yearLabels" :min="0" :grid="grid" :interactive="true"/>
-        <trend-chart  v-if="allDataset.length && isShown == 4" :datasets="[{data: allDataset, fill: true, className: 'curve-btc'}]" :labels="allLabels" :min="0" :grid="grid" :interactive="true"  @mouse-move="onMouseMove"/>
+        <trend-chart  v-if="tenYearDataset.length && isShown == 4" :datasets="[{data: tenYearDataset, fill: true, className: 'curve-btc'}]" :labels="tenYearLabels" :min="0" :grid="grid" :interactive="true"/>
 
         <div class = "historyButtons">
-            <button v-on:click="changeShown(1)">1 week</button>
-            <button v-on:click="changeShown(2)">1 month</button>
-            <button v-on:click="changeShown(3)">1 year</button>
-            <button v-on:click="changeShown(4)">10 years</button>
-
-            {{getDate()}}
+            <button v-on:click="changeShown(1), getLastWeeksData()">1 week</button>
+            <button v-on:click="changeShown(2), getLastMonthsData()">1 month</button>
+            <button v-on:click="changeShown(3), getLastYearsData()">1 year</button>
+            <button v-on:click="changeShown(4), getLastTenYearsData()">10 years</button>
         </div>
     </div>
 </template>
@@ -30,7 +28,7 @@
                 weekDataset: [],
                 monthDataset: [],
                 yearDataset: [],
-                allDataset: [],
+                tenYearDataset: [],
                 weekLabels: {
                     xLabels: [],
                     yLabels: 5,
@@ -46,7 +44,7 @@
                     yLabels: 5,
                     yLabelsTextFormatter: val => "$" + Math.round(val * 100) / 100
                 },
-                allLabels: {
+                tenYearLabels: {
                     xLabels: [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020],
                     yLabels: 5,
                     yLabelsTextFormatter: val => "$" + Math.round(val * 100) / 100
@@ -70,44 +68,67 @@
                         .subtract(years, "years")
                         .format("YYYY-MM-DD")
             },
+            getLastWeeksData(){
+                this.weekDataset = [];
+                this.weekLabels.xLabels = [];
+
+                axios
+                    .get("https://api.coindesk.com/v1/bpi/historical/close.json?start=" + this.getDate(7, 0, 0) + "&end=" + this.getDate(0, 0, 0))
+                    .then(res => {
+                        const data = res.data.bpi;
+                        for (let key in data) {
+                            this.weekDataset.push(data[key]);
+                            this.weekLabels.xLabels.push(moment(key).format("MM/DD"));
+                        }
+                    });
+            },
+            getLastMonthsData(){
+                this.monthDataset = [];
+                this.monthLabels.xLabels = [];
+
+                axios
+                    .get("https://api.coindesk.com/v1/bpi/historical/close.json")
+                    .then(res => {
+                        const data = res.data.bpi;
+                        for (let key in data) {
+                            this.monthDataset.push(data[key]);
+                            this.monthLabels.xLabels.push(moment(key).format("MM/DD"));
+                        }
+                    });
+            },
+            getLastYearsData(){
+                this.yearDataset = [];
+
+                axios
+                    .get("https://api.coindesk.com/v1/bpi/historical/close.json?start=" + this.getDate(0, 0, 1) + "&end=" + this.getDate(0, 0, 0))
+                    .then(res => {
+                        const data = res.data.bpi;
+                        for (let key in data) {
+                            this.yearDataset.push(data[key]);
+                        }
+                    });
+            },
+            getLastTenYearsData(){
+                this.tenYearDataset = [];
+                
+                axios
+                    .get("https://api.coindesk.com/v1/bpi/historical/close.json?start=" + this.getDate(0, 0, 10) + "&end=" + this.getDate(0, 0, 0))
+                    .then(res => {
+                        const data = res.data.bpi;
+                        for (let key in data) {
+                            this.tenYearDataset.push(data[key]);
+                        }
+                    });
+            }
 
         },
         mounted() {
-            axios
-                .get("https://api.coindesk.com/v1/bpi/historical/close.json?start=" + this.getDate(7, 0, 0) + "&end=" + this.getDate(0, 0, 0))
-                .then(res => {
-                    const data = res.data.bpi;
-                    for (let key in data) {
-                        this.weekDataset.push(data[key]);
-                        this.weekLabels.xLabels.push(moment(key).format("MM/DD"));
-                    }
-                });
-
-            axios
-                .get("https://api.coindesk.com/v1/bpi/historical/close.json")
-                .then(res => {
-                    const data = res.data.bpi;
-                    for (let key in data) {
-                        this.monthDataset.push(data[key]);
-                        this.monthLabels.xLabels.push(moment(key).format("MM/DD"));
-                    }
-                });
-
-            axios
-                .get("https://api.coindesk.com/v1/bpi/historical/close.json?start=" + this.getDate(0, 0, 1) + "&end=" + this.getDate(0, 0, 0))
-                .then(res => {
-                    const data = res.data.bpi;
-                    for (let key in data) {
-                        this.yearDataset.push(data[key]);
-                    }
-                });
-
             axios
                 .get("https://api.coindesk.com/v1/bpi/historical/close.json?start=" + this.getDate(0, 0, 10) + "&end=" + this.getDate(0, 0, 0))
                 .then(res => {
                     const data = res.data.bpi;
                     for (let key in data) {
-                        this.allDataset.push(data[key]);
+                        this.tenYearDataset.push(data[key]);
                     }
                 });
         }
